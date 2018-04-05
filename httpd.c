85d0dd96b80641765df6ab9bc67d6cfd1aad3cad
JessyLuk <lukxuan@foxmail.com>
2018-04-06 07:13:24 +0800

    add comment

--- httpd.c
+++ httpd.c
@@ -68,6 +68,7 @@ void accept_request(void *arg)
 
     numchars = get_line(client, buf, sizeof(buf));
     i = 0; j = 0;
+	/**从消息头中分割出是GET还是POST请求**/
     while (!ISspace(buf[i]) && (i < sizeof(method) - 1))
     {
         method[i] = buf[i];
@@ -75,9 +76,10 @@ void accept_request(void *arg)
     }
     j=i;
     method[i] = '\0';
-
+	
     if (strcasecmp(method, "GET") && strcasecmp(method, "POST"))
     {
+		/**既不是GET也不是POST，返回501**/
         unimplemented(client);
         return;
     }
@@ -87,49 +89,59 @@ void accept_request(void *arg)
 
     i = 0;
     while (ISspace(buf[j]) && (j < numchars))
-        j++;
-    while (!ISspace(buf[j]) && (i < sizeof(url) - 1) && (j < numchars))
+        j++;//过滤掉空格
+    while (!ISspace(buf[j]) && (i < sizeof(url) - 1) && (j < numchars))//找出GET后面的URL
     {
         url[i] = buf[j];
         i++; j++;
     }
     url[i] = '\0';
 
-    if (strcasecmp(method, "GET") == 0)
+    if (strcasecmp(method, "GET") == 0)//如果是GET请求
     {
         query_string = url;
+		//找在URL中使用GET传送来的参数
         while ((*query_string != '?') && (*query_string != '\0'))
             query_string++;
+		//如果真有参数，就需要执行CGI来处理
         if (*query_string == '?')
         {
             cgi = 1;
-            *query_string = '\0';
-            query_string++;
+            *query_string = '\0';//从?截断
+            query_string++;//保存参数的首地址
         }
     }
 
-    sprintf(path, "htdocs%s", url);
+    sprintf(path, "htdocs%s", url);//把/修改成相对于服务器的路径“/”-->"htdocs/"
     if (path[strlen(path) - 1] == '/')
-        strcat(path, "index.html");
-    if (stat(path, &st) == -1) {
+        strcat(path, "index.html");//如果以“#/”结尾，自动补成“#/index.html”
+    if (stat(path, &st) == -1) {//如果读取文件信息失败
+		//stat失败，首先把client消息头都读完
         while ((numchars > 0) && strcmp("\n", buf))  /* read & discard headers */
             numchars = get_line(client, buf, sizeof(buf));
-        not_found(client);
+        not_found(client);//返回404页面
     }
     else
-    {
+    {//stat成功，有这个文件
+		//如果这个文件就是头文件，就默认请求的是这个文件夹的index.html
         if ((st.st_mode & S_IFMT) == S_IFDIR)
             strcat(path, "/index.html");
+		//如果请求的文件是可执行文件
         if ((st.st_mode & S_IXUSR) ||
                 (st.st_mode & S_IXGRP) ||
                 (st.st_mode & S_IXOTH)    )
-            cgi = 1;
+            cgi = 1;//再次把CGI设置成1
         if (!cgi)
-            serve_file(client, path);
+            serve_file(client, path);//如果cgi是0，传入的cgi文件路径和sock文件描述符
         else
-            execute_cgi(client, path, method, query_string);
+            execute_cgi(client, path, method, query_string);//参数依此为：client,cgi文件路径，请求方法，?后面跟的参数
     }
-
+	/**
+	close:关闭读写。成功则返回0，错误返回-1，错误码errno：EBADF表示fd不是一个有效描述符；EINTR表示close函数被信号中断；EIO表示一个IO错误。
+	close和shutdown区别：
+		　close-----关闭本进程的socket id，但链接还是开着的，用这个socket id的其它进程还能用这个链接，能读或写这个socket id。
+　　shutdown--破坏了socket 链接，读的时候可能侦探到EOF结束符，写的时候可能会收到一个SIGPIPE信号，这个信号可能直到socket buffer被填充了才收到，shutdown有一个关闭方式的参数，0 不能再读，1不能再写，2 读写都不能。
+	**/
     close(client);
 }
 
@@ -221,6 +233,7 @@ void execute_cgi(int client, const char *path,
     int content_length = -1;
 
     buf[0] = 'A'; buf[1] = '\0';
+	//如果是GET请求，那么参数就在URL中传递，已经提取到了query_string，把剩下的消息头丢掉
     if (strcasecmp(method, "GET") == 0)
         while ((numchars > 0) && strcmp("\n", buf))  /* read & discard headers */
             numchars = get_line(client, buf, sizeof(buf));
@@ -230,12 +243,13 @@ void execute_cgi(int client, const char *path,
         while ((numchars > 0) && strcmp("\n", buf))
         {
             buf[15] = '\0';
+			//把内容按行读，筛选出Content-Length这个字段，得到消息实体的长度
             if (strcasecmp(buf, "Content-Length:") == 0)
                 content_length = atoi(&(buf[16]));
             numchars = get_line(client, buf, sizeof(buf));
         }
-        if (content_length == -1) {
-            bad_request(client);
+        if (content_length == -1) {//Content-Length=-1，说明消息头中没有Content-Length这个字段
+            bad_request(client);//给前端请求返回400，请求出现语法错误
             return;
         }
     }
@@ -243,7 +257,7 @@ void execute_cgi(int client, const char *path,
     {
     }
 
-
+	/**创建与子进程通信的管道**/
     if (pipe(cgi_output) < 0) {
         cannot_execute(client);
         return;
@@ -264,24 +278,24 @@ void execute_cgi(int client, const char *path,
         char meth_env[255];
         char query_env[255];
         char length_env[255];
-
+		//把子进程的标准输入和标准输出都重定向到管道上进行读参数，写数据的操作
         dup2(cgi_output[1], STDOUT);
         dup2(cgi_input[0], STDIN);
         close(cgi_output[0]);
         close(cgi_input[1]);
         sprintf(meth_env, "REQUEST_METHOD=%s", method);
-        putenv(meth_env);
-        if (strcasecmp(method, "GET") == 0) {
+        putenv(meth_env);//拼接回应的请求方式，添加meth_env到子进程的环境变量中
+        if (strcasecmp(method, "GET") == 0) {//如果是GET请求，就把从URL中提取的参数放到QUERY_STRING这个环境变量里
             sprintf(query_env, "QUERY_STRING=%s", query_string);
             putenv(query_env);
         }
-        else {   /* POST */
+        else {   /* POST *///POST把消息实体的长度设置为环境变量
             sprintf(length_env, "CONTENT_LENGTH=%d", content_length);
             putenv(length_env);
         }
-        execl(path, NULL);
+        execl(path, NULL);//execl把cgi的这个应用拉起来
         exit(0);
-    } else {    /* parent */
+    } else {    /* parent *///关闭不需要的管道读写端
         close(cgi_output[1]);
         close(cgi_input[0]);
         if (strcasecmp(method, "POST") == 0)
@@ -321,9 +335,10 @@ int get_line(int sock, char *buf, int size)
     {
         n = recv(sock, &c, 1, 0);
         /* DEBUG printf("%02X\n", c); */
-        if (n > 0)
+		//一个字节一个字节从sock中读取内容
+        if (n > 0)//读到内容
         {
-            if (c == '\r')
+            if (c == '\r')//读到'\r'就代表这一行结束，如果后面有\n就读出来，如果没有就自己加上'\n'，MSG_PEEK返回的数据并不会在系统内删除，如果再次调用recv()会返回相同的数据
             {
                 n = recv(sock, &c, 1, MSG_PEEK);
                 /* DEBUG printf("%02X\n", c); */
@@ -430,28 +445,79 @@ int startup(u_short *port)
 {
     int httpd = 0;
     int on = 1;
+	/**
+		struct sockaddr 这个结构体是linux的网络编程接口中用来表示IP地址的标准结构体，
+		bind、connect等函数中都需要这个结构体，这个结构体是兼容IPV4和IPV6的。
+		在实际编程中这个结构体会被一个struct sockaddr_in所填充。
+		sockaddr_in在头文件#include<netinet/in.h>或#include <arpa/inet.h>中定义，
+		该结构体解决了sockaddr的缺陷，把port和addr 分开储存在两个变量中
+	**/
     struct sockaddr_in name;
+	/**
+	Windows中PF_INET和AF_INET是相同概念：
+					AF=Address Family,PF=protocol family
+					Winsock2.h中：
+							#define AF_INET 0
+							#define PF_INET AF_INET
+	Unix/Linux系统中：
+			BSD是AF，对于POSIX是PF
+			理论上建立socket时是指定协议，应该用PF_xxxx，设置地址时应该用AF_xxxx。
+			当然AF_INET和PF_INET的值是相同的，混用也不会有太大的问题。也就是说你socket时候用PF_xxxx，
+			设置的时候用AF_xxxx也是没关系的，这点随便找个TCPIP例子就可以验证出来了。如下，不论是AF_INET
+			还是PF_INET都是可行的，只不过这样子的话，有点不符合规范。
 
+
+			SOCK_STREAM：流式套接字
+			SOCK_DGRAM：数据报套接字
+	**/
     httpd = socket(PF_INET, SOCK_STREAM, 0);
     if (httpd == -1)
         error_die("socket");
     memset(&name, 0, sizeof(name));
     name.sin_family = AF_INET;
-    name.sin_port = htons(*port);
-    name.sin_addr.s_addr = htonl(INADDR_ANY);
-    if ((setsockopt(httpd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on))) < 0)  
+    name.sin_port = htons(*port);//将主机的无符号短整形数转换成网络字节顺序，参数为hostshort:主机字节顺序表达的16位数
+   //INADDR_ANY就是指定地址为0.0.0.0的地址，这个地址事实上表示不确定地址，或“所有地址”、“任意地址”。 一般来说，在各个系统中均定义成为0值
+	name.sin_addr.s_addr = htonl(INADDR_ANY);//将主机数转换成无符号长整形的网络字节顺序。本函数将一个32位数从主机字节顺序转换成网络字节顺序，参数位hostlong:主机字节顺序表达的32位数
+    /**
+	setsockopt()用于任意类型，任意状态套接口的设置选项值。尽管在不同协议上存在选项，但本函数仅定义了最高的“套接口”层次上的选项
+	参数依此为：
+		socket structure
+		option level:想要在套接字界别上设置选项，就必须把level设置为SOL_SOCKET
+		option identifie：
+		pointer to new option value
+		length of option value
+
+		SO_REUSEADDR:这个套接字选项通知内核，如果端口忙，但TCP状态位于 TIME_WAIT ，可以重用端口。如果端口忙，而TCP状态位于其他状态，重用端口时依旧得到一个错误信息，指明"地址已经使用中"。如果你的服务程序停止后想立即重启，而新套接字依旧使用同一端口，此时 SO_REUSEADDR 选项非常有用。必须意识到，此时任何非期望数据到达，都可能导致服务程序反应混乱，不过这只是一种可能，事实上很不可能。
+	**/
+	if ((setsockopt(httpd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on))) < 0)  
     {  
         error_die("setsockopt failed");
     }
+	/**
+	bind()服务端套接字绑定自己的IP地址与端口号，客户端那边可以不写，内核会给它分配一个临时的端口号
+	**/
     if (bind(httpd, (struct sockaddr *)&name, sizeof(name)) < 0)
         error_die("bind");
     if (*port == 0)  /* if dynamically allocating a port */
     {
         socklen_t namelen = sizeof(name);
+		/**
+		getsockname()：获取本地套接口的名字，包括它的IP和端口
+		getpeername()：获取远程套接口的名字，包括他的IP和端口
+		**/
         if (getsockname(httpd, (struct sockaddr *)&name, &namelen) == -1)
             error_die("getsockname");
         *port = ntohs(name.sin_port);
     }
+	/**
+	listen函数使用主动连接套接口变为被连接套接口，使得一个进程可以接受其他进程的请求，从而称为一个服务器进程。在TCP服务器编程中listen函数把
+	进程变为一个服务器，并指定相应的套接字变为背动连接。
+	listen函数一般在调用bind之后-调用accept之前调用。返回0---成功，-1--失败。
+	参数依此为：
+	sockfd:被listen函数作用的套接字，sockfd之前由socket函数返回。在被socket函数返回的套接字fd之时，它是一个主动连接的套接字，也就是此时系统假设用户会对这个套接字调用connect函数，期待它主动与其它进程连接，然后在服务器编程中，用户希望这个套接字可以接受外来的连接请求，也就是被动等待用户来连接。由于系统默认时认为一个套接字是主动连接的，所以需要通过某种方式来告诉系统，用户进程通过系统调用listen来完成这件事。
+	backlog:这个参数涉及到一些网络的细节。在进程正理一个一个连接请求的时候，可能还存在其它的连接请求。因为TCP连接是一个过程，所以可能存在一种半连接的状态，有时由于同时尝试连接的用户过多，使得服务器进程无法快速地完成连接请求。如果这个情况出现了，服务器进程希望内核如何处理呢？内核会在自己的进程空间里维护一个队列以跟踪这些完成的连接但服务器进程还没有接手处理或正在进行的连接，这样的一个队列内核不可能让其任意大，所以必须有一个大小的上限。这个backlog告诉内核使用这个数值作为上限。
+毫无疑问，服务器进程不能随便指定一个数值，内核有一个许可的范围。这个范围是实现相关的。很难有某种统一，一般这个值会小30以内。
+	**/
     if (listen(httpd, 5) < 0)
         error_die("listen");
     return(httpd);
@@ -500,6 +566,11 @@ int main(void)
 
     while (1)
     {
+		/**
+		accept函数主要用于服务器端，一般位于listen函数之后，默认会阻塞进程，直到有一个客户请求连接，建立好连接后，
+		它返回的一个新的套接字socketfd_new，此后服务器端可使用这个新的套接字socketfd_new与该客户进行通信，而sockfd则
+		继续用于监听其他客户端的连接请求。
+		**/
         client_sock = accept(server_sock,
                 (struct sockaddr *)&client_name,
                 &client_name_len);
